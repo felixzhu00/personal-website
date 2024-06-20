fee5fe8e86eadc5d75c935003f45b847b63e5074
felixzhu00 <felix.zhu00@gmail.com>
2024-06-20 18:06:31 -0400

    attach page switching function to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-function App() {
-  return <div className="p-4 pt-2">Hello Word</div>
-}
+// src/App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SectionRefsProvider } from './components/SectionRefsContext';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import ScrollToSection from './components/ScrollToSection';
 
-export default App
+const App: React.FC = () => (
+  <SectionRefsProvider>
+    <Router>
+      <ScrollToSection />
+      <div className="flex h-screen">
+        <Navigation />
+        <div className="flex-1 overflow-auto">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  </SectionRefsProvider>
+);
+
+export default App;
