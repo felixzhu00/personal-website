b256353525b29e7b52b7c3130bb94f3dbf4d8238
felixzhu00 <felix.zhu00@gmail.com>
2024-06-20 18:01:38 -0400

    experiment with tailwind config for custom var

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1C1E1A', // Replace with your desired primary color
+        secondary: '#31352E', // Replace with your desired secondary color
+        accent: '#EBEBE8'
+      },
+      fontFamily: {
+        sans: ['Inter', 'Arial', 'sans-serif'], // Replace 'Inter' with your custom font name
+      },
+      fontWeight: {
+        '33': '33',
+      },
+    },
   },
   plugins: [],
 }
