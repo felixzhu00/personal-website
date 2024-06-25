44e4d6ccada5a2ea56414abd73f9e620b1d46712
felixzhu00 <felix.zhu00@gmail.com>
2024-06-24 20:51:20 -0400

    complete home page layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,23 @@
-import { forwardRef } from 'react';
+import { forwardRef } from 'react'
 
-interface HomeProps {
-}
+interface HomeProps {}
 
 const Home = forwardRef<HTMLElement, HomeProps>((props, ref) => (
-  <section id="home" ref={ref} className="section">
-    <h2>Home Section</h2>
+  <section
+    id="home"
+    ref={ref}
+    className="section text-textBase ml-[20%] flex h-full w-4/5 items-center justify-center bg-primary text-xl font-semibold"
+  >
+    <div className="flex flex-col items-center text-center">
+      <h2>Felix Zhu</h2>
+      <h2>Software Developer</h2>
+      <p className='w-2/3'>
+        Welcome to my personal website! Here you can learn more about me. Feel
+        free to explore my projects, skills, and contact information.
+      </p>
+      <button className="w-[150px] bg-textBase p-3 rounded-2xl"> <span className='text-primary'>Learn More</span></button>
+    </div>
   </section>
-));
+))
 
-export default Home;
-
+export default Home
