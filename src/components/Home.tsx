92e5ba8b3bfa8e6697e4d038ee57f651d2458947
felixzhu00 <felix.zhu00@gmail.com>
2024-06-29 19:22:17 -0400

    Change homepage design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,40 @@
-import { forwardRef } from 'react'
-
-interface HomeProps {}
-
-const Home = forwardRef<HTMLElement, HomeProps>((props, ref) => (
-  <section
-    id="home"
-    ref={ref}
-    className="pg-style items-center justify-center "
-  >
-    <div className="flex flex-col items-center text-center">
-      <h2>Felix Zhu</h2>
-      <h2>Software Developer</h2>
-      <p className="w-2/3">
-        Welcome to my personal website! Here you can learn more about me. Feel
-        free to explore my projects, skills, and contact information.
-      </p>
-      <button className="w-[150px] rounded-2xl bg-textBase p-3">
-        <span className="text-primary">Learn More</span>
-      </button>
-    </div>
-  </section>
-))
+const Home: React.FC = () => {
+  return (
+    <section className="flex h-full w-full flex-col items-start justify-center">
+      <div>
+        <h1 className="mb-[30px] ml-[4px] text-secondary">Hi, my name is</h1>
+      </div>
+      <div>
+        <h2 className="m-0 mb-[10px] ml-[4px] text-[clamp(40px,8vw,80px)] font-semibold leading-[1.1] text-textBase">Felix Zhu.</h2>
+      </div>
+      <div>
+        <h3 className="text-[clamp(40px,8vw,80px)] font-semibold leading-[0.9] text-textAccent">Software Developer.</h3>
+      </div>
+      <div>
+        <p className="mt-[30px] ml-[4px] text-textAccent max-w-lg">
+          Welcome to my personal website! Here you can learn more about me. Feel free to explore my projects, skills, and contact information.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  )
+}
 
 export default Home
+// <section
+//   id="home"
+//   ref={ref}
+//   className="pg-style items-center justify-center "
+// >
+//   <div className="flex flex-col items-center text-center">
+//     <h2>Felix Zhu</h2>
+//     <h2>Software Developer</h2>
+//     <p className="w-2/3">
+//       Welcome to my personal website! Here you can learn more about me. Feel
+//       free to explore my projects, skills, and contact information.
+//     </p>
+//     <button className="w-[150px] rounded-2xl bg-textBase p-3">
+//       <span className="text-primary">Learn More</span>
+//     </button>
+//   </div>
+// </section>
