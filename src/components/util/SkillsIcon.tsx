4919a5af787ecc7f2d0e152db97876889eda0155
felixzhu00 <felix.zhu00@gmail.com>
2024-06-30 18:58:34 -0400

    change skillicon px size

--- src/components/util/SkillsIcon.tsx
+++ src/components/util/SkillsIcon.tsx
@@ -9,7 +9,7 @@ interface SkillsIconProps {
 }
 const SkillsIcon = ({ className, color, icon, text }: SkillsIconProps) => {
   return (
-    <div className='flex flex-col items-center rounded bg-[#112240] text-center justify-between aspect-square h-[15vh] p-4'>
+    <div className='flex flex-col items-center rounded bg-[#112240] text-center justify-between aspect-square h-[150px] p-4'>
         <SVGIcon SvgComponent={icon} className={`h-1/2 w-1/2 grow ${className}`} color={color} />
       <p className="text-[13px] m-0 w-full text-textAccent font-sans font-semibold">{text}</p>
     </div>
