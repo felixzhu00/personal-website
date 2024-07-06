c9a46d985475b7b8cf6e19f2da706ee07bf15ce8
felixzhu00 <felix.zhu00@gmail.com>
2024-07-06 16:56:54 -0400

    edit styling of home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,10 +10,10 @@ const Home: React.FC = () => {
   }
   return (
     <section id="home" className="section mx-auto flex min-h-[100vh] w-full flex-col items-start justify-center">
-      <h1 className="mb-[30px] ml-[8px] font-mono font-normal leading-[1.1] text-secondary">Hi, my name is</h1>
-      <h2 className="m-0 mb-[10px] ml-[4px] text-[clamp(40px,8vw,80px)] font-semibold leading-[1.1] text-textBase">Felix Zhu.</h2>
+      <h1 className="mb-[30px] font-mono font-normal leading-[1.1] text-secondary">Hi, my name is</h1>
+      <h2 className="m-0 mb-[10px]  text-[clamp(40px,8vw,80px)] font-semibold leading-[1.1] text-textBase">Felix Zhu.</h2>
       <h3 className="text-[clamp(40px,8vw,80px)] font-semibold leading-[0.9] text-textAccent">Software Developer.</h3>
-      <p className="ml-[4px] mt-[30px] max-w-lg text-textAccent">
+      <p className="mt-[30px] max-w-lg text-textAccent">
         Welcome to my personal website! Here you can learn more about me. Feel free to explore my projects, skills, and contact information.
       </p>
       <button className="mt-[50px] rounded border border-secondary px-4 py-3 font-mono text-secondary">
