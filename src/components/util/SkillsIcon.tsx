102cd7f0559e8cc9dc3f577db1348008393727da
felixzhu00 <felix.zhu00@gmail.com>
2024-07-06 17:09:07 -0400

    change style of skills page

--- src/components/util/SkillsIcon.tsx
+++ src/components/util/SkillsIcon.tsx
@@ -11,7 +11,7 @@ const SkillsIcon = ({ className, color, icon, text }: SkillsIconProps) => {
   return (
     <div className="flex aspect-square h-[172px] flex-col items-center justify-between rounded bg-tri p-4 text-center">
       <SVGIcon SvgComponent={icon} className={`h-1/2 w-1/2 grow ${className}`} color={color} />
-      <p className="m-0 w-full font-sans text-[22px] font-semibold text-textAccent sm:text-[17px]">{text}</p>
+      <p className="m-0 w-full font-sans text-[19px] font-semibold text-textAccent sm:text-[17px]">{text}</p>
     </div>
   )
 }
