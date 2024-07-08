8f95f363a1a6fc74bfa819366c54a005be225f78
felixzhu00 <felix.zhu00@gmail.com>
2024-07-08 00:27:37 -0400

    gh pages implementation

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personal-website/",
   plugins: [react(),svgr()],
 })
