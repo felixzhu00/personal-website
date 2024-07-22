fed82f4debe36c0cb645eb6c137e0471284401ba
felixzhu00 <felix.zhu00@gmail.com>
2024-07-22 18:48:41 -0400

    change base again

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [react(),svgr()],
-  base: "/personal-website/"
 })
