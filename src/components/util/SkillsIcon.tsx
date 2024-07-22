b552cfea0184bde5c7bfbe76a4c12a87e4708ebd
felixzhu00 <felix.zhu00@gmail.com>
2024-07-22 18:25:39 -0400

    remove unused param in SkillsIcon

--- src/components/util/SkillsIcon.tsx
+++ src/components/util/SkillsIcon.tsx
@@ -7,7 +7,7 @@ interface SkillsIconProps {
   text?: string
   icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
 }
-const SkillsIcon = ({ className, color, icon, text }: SkillsIconProps) => {
+const SkillsIcon = ({ className, icon, text }: SkillsIconProps) => {
   return (
     <div className="flex aspect-square h-[172px] flex-col items-center justify-between rounded bg-tri p-4 text-center">
       <SVGIcon SvgComponent={icon} className={`h-1/2 w-1/2 grow ${className}`} strokeColor={text == "Bash Script" ? "var(--color-text-accent)" : ""}/>
