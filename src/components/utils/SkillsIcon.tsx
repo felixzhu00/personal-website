1e31bbb51da8a9f8596fb90e822bf4f624fcf0c5
felixzhu00 <felix.zhu00@gmail.com>
2024-10-01 17:14:17 -0400

    fix skillicon mobile scaling

--- src/components/utils/SkillsIcon.tsx
+++ src/components/utils/SkillsIcon.tsx
@@ -9,9 +9,9 @@ interface SkillsIconProps {
 }
 const SkillsIcon = ({ className, icon, text }: SkillsIconProps) => {
   return (
-    <div className="h-50 flex aspect-square flex-col items-center justify-between rounded-md bg-background-100 p-4 text-center shadow-lg sm:h-48">
+    <div className="h-56 flex aspect-square flex-col items-center justify-between rounded-md bg-background-100 p-4 text-center shadow-lg sm:h-48">
       <SVGIcon iconName={icon} SvgComponent={iconMap[icon]} className={`h-1/2 w-1/2 grow ${className}`} />
-      <p className="paragraph m-0 w-full font-sans font-semibold sm:text-lg">{text}</p>
+      <p className=" m-0 w-full font-sans font-semibold text-lg">{text}</p>
     </div>
   )
 }
